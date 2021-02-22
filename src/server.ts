b50bbbfd03cf76c27fff15b4256a397c0532f915
Nayara Ribeiro Freire <nayfreireof@gmail.com>
2021-02-22 14:20:17 -0300

    Rotas

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from 'express'
 
 const app = express()
 
+app.get('/', (request, response) => {
+    return response.json({message: 'Hello World - NLW #4'})
+})
+
+app.post('/', (request, response) => {
+    return response.json({message: 'Os dados foram salvos com sucesso'})
+})
+
 app.listen(4222, () => console.log("O pai tá on"))
