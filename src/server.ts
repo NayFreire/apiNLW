21a65409b7a1b60d08408730b10dd2dd91248c5d
Nayara Ribeiro Freire <nayfreireof@gmail.com>
2021-02-23 10:50:44 -0300

    Criando a função create para criar um novo usuário, criando o arquivo de rotas e o importando no server

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,10 @@
 import 'reflect-metadata'
 import express from 'express'
 import './database' //importa o arquivo index.ts por padrão
+import { router } from './routes'
 
 const app = express()
 
-app.get('/', (request, response) => {
-    return response.json({message: 'Hello World - NLW #4'})
-})
-
-app.post('/', (request, response) => {
-    return response.json({message: 'Os dados foram salvos com sucesso'})
-})
+app.use(router)
 
 app.listen(4222, () => console.log("O pai tá on"))
