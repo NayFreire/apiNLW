d185ef6c7b34ddace167718eedc8490a8a9a747a
Nayara Ribeiro Freire <nayfreireof@gmail.com>
2021-02-23 14:32:39 -0300

    Verificando se um email de usuário já foi cadastrado no sistema

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -5,6 +5,14 @@ class UserController{
     async create(request: Request, response: Response){
         const {name, email} = request.body;
         const usersRepository = getRepository(User); //Repositórios fazem a conexão com o banco de dados atravéz das operações (insert, delete, update, select)
+        
+        const userAlreadyExists = await usersRepository.findOne({email}) //Verificação para ver se já está cadastrado um usuário com esse email
+
+        if(userAlreadyExists){ //Caso esteja cadastrado, um erro é mostrado
+            return response.status(400).json({
+                error: "User already exists"
+            })
+        }
 
         const user = usersRepository.create({name, email}) //Cria o usuário
 
